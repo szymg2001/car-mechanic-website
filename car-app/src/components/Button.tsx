4cb60898bf363f04bd046a94191c5921bf118128
szymg2001 <szymon.grabowiec2001@gmail.com>
2024-07-28 08:04:50 +0200

    Created Button component

---
+++ car-app/src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type ButtonProps = {
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+export default function Button({ onClick, children }: ButtonProps) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
