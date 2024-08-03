1fcdb9eee13391e072fc3aa0c92e3d3a434f8b0b
szymg2001 <szymon.grabowiec2001@gmail.com>
2024-08-03 19:50:06 +0200

    Updated Logo - divided text into two parts - main text and lower text

--- car-app/src/components/Logo.tsx
+++ car-app/src/components/Logo.tsx
@@ -1,3 +1,8 @@
 export default function Logo() {
-  return <p className="logo">Lorem Ipsum</p>;
+  return (
+    <p className="logo">
+      <span className="logo__main-text">Patryk Kobos</span>
+      <span className="logo__lower-text">Mechanika samochodowa</span>
+    </p>
+  );
 }
